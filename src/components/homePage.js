34f30a123d1aa5bb1bf68357498c5b29684b8223
Tsinat Zeree <cheru87@gmail.com>
2016-08-06 22:30:17 -0700

    initial setup

---
+++ src/components/homePage.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react');
+var Home = React.createClass({
+    render: function() {
+        return (
+            <div className='jumbotron'>
+                <h1>PLURALSIGHT ADMINISTRATION</h1>
+                <p>Rect, Rect Router, and Flux for ultra-responsive web apps.</p>
+            </div>
+        );
+    }
+});
+
+module.exports = Home;
---
+++ src/main.js
@@ -0,0 +1,32 @@
+$ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
+
+(function(win) {
+    'use strict';
+
+    var App = React.createClass({
+        render: function() {
+            var Child;
+
+            switch(this.props.route) {
+                case 'about': Child = About; break;
+                default: Child = Home;
+            }
+
+            return (
+                <div>
+                    <Child/>
+                </div>
+            );
+        }
+    });
+    function render() {
+        var route = window.location.hash.substr(1);
+        React.render(<App route={route}/>, document.getElementById('app'));
+    }
+
+    win.addEventListener('hashchange', render);
+    render();
+}(window));
